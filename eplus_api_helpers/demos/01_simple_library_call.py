30e81b1d1bb036a6b61a7a3ef0c13c36acbe9561
Edwin Lee <leeed2001@gmail.com>
2023-02-03 13:50:21 -0600

    Add command line arg for eplus path, add GHA workflow to test

--- eplus_api_helpers/demos/01_simple_library_call.py
+++ eplus_api_helpers/demos/01_simple_library_call.py
@@ -1,8 +1,13 @@
 from pathlib import Path
+from sys import argv
 from eplus_api_helpers.import_helper import EPlusAPIHelper
 
 
-e = EPlusAPIHelper(Path('/eplus/installs/EnergyPlus-22-2-0'))
+eplus_path = '/eplus/installs/EnergyPlus-22-2-0'
+if len(argv) > 1:
+    eplus_path = argv[1]
+
+e = EPlusAPIHelper(Path(eplus_path))
 api = e.get_api_instance()
 state = api.state_manager.new_state()
 return_value = api.runtime.run_energyplus(
