25c53d505bbe766a25d166836969def8eec4c70d
Edwin Lee <leeed2001@gmail.com>
2023-02-03 14:54:34 -0600

    Try some stuff

--- eplus_api_helpers/demos/02_threaded.py
+++ eplus_api_helpers/demos/02_threaded.py
@@ -1,10 +1,12 @@
 from pathlib import Path
+from sys import argv
 from threading import Thread
 from eplus_api_helpers.import_helper import EPlusAPIHelper
 
 
-e = EPlusAPIHelper(Path('/eplus/installs/EnergyPlus-22-2-0'))
-api = e.get_api_instance()
+eplus_path = '/eplus/installs/EnergyPlus-22-2-0'
+if len(argv) > 1:
+    eplus_path = argv[1]
 
 
 def thread_function(_working_dir: str):
@@ -22,6 +24,8 @@ def thread_function(_working_dir: str):
     )
 
 
+e = EPlusAPIHelper(Path(eplus_path))
+api = e.get_api_instance()
 for index in range(3):
     working_dir = e.get_temp_run_dir()
     print(f"Main    : create and start thread at working directory: {working_dir}.")
